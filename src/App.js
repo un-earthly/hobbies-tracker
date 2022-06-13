03b1fcdb2202f28baa27e2db5f41efe52a8d0bee
un-earthly <vijayalamin@gmail.com>
2022-06-13 11:36:55 -0700

    form and table added wth json placeholder data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,74 @@
-import { useState } from 'react';
+import { PlusIcon } from '@heroicons/react/solid';
+import { useForm } from "react-hook-form";
 import Table from './components/Table'
 function App() {
-  const [theme, setTheme] = useState("dark")
+  const { handleSubmit, register, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
   return (
     <div>
       <Table />
+
+      <div className="flex items-center justify-center my-10">
+        <label for="addmore" className="btn btn-modal w-3/4" >Add More <PlusIcon height={18} /></label>
+      </div>
+      <input type="checkbox" id="addmore" class="modal-toggle" />
+      <div class="modal">
+        <div class="modal-box pb-14 relative">
+          <label for="addmore" class="btn btn-sm btn-circle btn-error absolute right-2 top-2">✕</label>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <h1 className='text-2xl text-center my-4'>Please Fill Up the form</h1>
+            <div className='w-3/4 mx-auto space-y-3'>
+              <div>
+                <input
+                  className='input w-full border-2 border-gray-300 focus:border-green-300 focus:outline-none'
+                  placeholder="Please enter your name"
+                  {...register("name", {
+                    required: "Please Enter Your Name"
+                  })}
+                />
+                {errors.name && <p className='my-3 text-red-500'>{errors.name.message}</p>}
+              </div>
+              <div>
+                <input
+                  type="email"
+                  className='input w-full border-2 border-gray-300 focus:border-green-300 focus:outline-none'
+                  placeholder="Please enter your name"
+                  {...register("email", {
+                    required: "Please enter a valid email",
+                    pattern: {
+                      value: /\S+@\S+\.\S+/
+                    }
+                  })}
+                />
+                {errors.email && <p className='my-3 text-red-500'>{errors.email.message}</p>}
+              </div>
+              <div>
+                <input
+                  className='input w-full border-2 border-gray-300 focus:border-green-300 focus:outline-none'
+                  placeholder="Please enter your name"
+                  {...register("phone", {
+                    required: "Please Enter Your Phone"
+                  })}
+                />
+                {errors.phone && <p className='my-3 text-red-500'>{errors.phone.message}</p>}
+              </div>
+              <div>
+                <input
+                  className='input w-full border-2 border-gray-300 focus:border-green-300 focus:outline-none'
+                  placeholder="Please enter your name"
+                  {...register("hobbies", {
+                    required: "Please Enter Your Hobbies"
+                  })}
+                />
+                {errors.hobbies && <p className='my-3 text-red-500'>{errors.hobbies.message}</p>}
+              </div>
+              <button className='btn btn-outline btn-success w-full'>
+                Save
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,27 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { TrashIcon, PencilIcon } from '@heroicons/react/solid'
+import axios from 'axios'
+import { useQuery } from 'react-query'
 export default function Table() {
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    const { isLoading, error, data, refetch } = useQuery('userData', () => axios.get('http://jsonplaceholder.typicode.com/users').then(res => res.data))
+    const getrowData = id => {
+        const url = `https://jsonplaceholder.typicode.com/users/${id}`
+        axios.get(url).then(res => console.log(res.data))
+    }
+    const handleDelete = id => {
+        const url = `https://jsonplaceholder.typicode.com/users/${id}`
+        axios.delete(url).then(res => console.log(res.data))
+
+    }
+
     return (
         <div class="overflow-x-auto">
             <table class="table w-full">
                 <thead>
                     <tr>
+                        <th>Select row</th>
                         <th>id</th>
                         <th>Name</th>
                         <th>phone</th>
@@ -16,24 +32,25 @@ export default function Table() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <th>1</th>
-                        <td>Cy Ganderton</td>
-                        <td>Quality Control Specialist</td>
-                        <td>Blue</td>
-                    </tr>
-                    <tr>
-                        <th>2</th>
-                        <td>Hart Hagerty</td>
-                        <td>Desktop Support Technician</td>
-                        <td>Purple</td>
-                    </tr>
-                    <tr>
-                        <th>3</th>
-                        <td>Brice Swyre</td>
-                        <td>Tax Accountant</td>
-                        <td>Red</td>
-                    </tr>
+                    {
+                        isLoading ? <p>Loading</p> :
+                            data.map(d => (
+                                <tr key={d.id}>
+                                    <td><input type="checkbox" class="checkbox" onClick={() => getrowData(d.id)} /></td>
+                                    <td>{d.id}</td>
+                                    <td>name</td>
+                                    <td>{d.phone}</td>
+                                    <td>{d.email}</td>
+                                    <td>{d.company.name}</td>
+                                    <td>
+
+                                        <label for="addmore" ><PencilIcon className='hover:text-gray-500 duration-300' height={25} /></label>
+                                    </td>
+
+                                    <td><TrashIcon className='hover:text-gray-500 duration-300' height={25} onClick={() => handleDelete(d.id)} /></td>
+                                </tr>
+                            ))
+                    }
                 </tbody>
             </table>
         </div>
